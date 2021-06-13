ba98008a1b40051a230e24621dfada0f368a7fd0
ssmooncoder <moonshamu@gmail.com>
2021-06-13 07:26:58 -0700

    try async on removal

--- popup.js
+++ popup.js
@@ -8,53 +8,66 @@ let inputs = document.querySelectorAll("input.entry-field");
 for (let input of inputs) {
   input.addEventListener("keydown", function(event) {
     if (event.keyCode === 13) {
-      addEntry();
+      addNewEntry();
     } else if (event.keyCode === 32) {
       event.preventDefault();
     }
   });
 }
 
-// let status = !("true" === e.currentTarget.getAttribute("expanded"))
-// e.currentTarget.setAttribute("expanded", status ? "true" : "false");
-
-addButton.addEventListener("click", (e) => {
+function addNewEntry() {
   let formData = new FormData(addForm);
   let data = Object.fromEntries(formData);
   let key = data["entry-key"];
   let value = data["entry-value"];
   
   chrome.storage.local.set({ [key]: value });
-  repopulate();
-});
+  repopulate();  
+}
 
-// clear.addEventListener("click", () => {
-//   chrome.storage.local.clear();
+addButton.addEventListener("click", addNewEntry);
+
+// addButton.addEventListener("click", (e) => {
+//   let formData = new FormData(addForm);
+//   let data = Object.fromEntries(formData);
+//   let key = data["entry-key"];
+//   let value = data["entry-value"];
+  
+//   chrome.storage.local.set({ [key]: value });
 //   repopulate();
 // });
 
-removeButton.addEventListener("click", function(event) {
-  let checkedList = document.querySelectorAll('input:checked');
+async function removeSelectedEntries() {
+  let entries = document.querySelectorAll('div[data="local-storage"] input.mark-remove');
   
-  let extracted = [...checkedList].map(checked => checked.name);
+  let checkedList = [...entries].filter(entry => entry.checked);
   
-  chrome.storage.local.remove(extracted, function() {
-    repopulate();
-  });
+  let keys = checkedList.map(checked => checked.name);
+  
+  await chrome.storage.local.remove(keys, repopulate);
   // repopulate();
-});
+}
+
+removeButton.addEventListener("click", removeSelectedEntries);
+
+// removeButton.addEventListener("click", function(event) {
+//   let entries = document.querySelectorAll('div[data="local-storage"] input.mark-remove');
+  
+//   let checkedList = [...entries].filter(entry => entry.checked);
+  
+//   let keys = checkedList.map(checked => checked.name);
+  
+//   chrome.storage.local.remove(keys, () => repopulate);
+//   repopulate();
+// });
 
 function handleRemove(e) {
+  let entries = document.querySelectorAll('div[data="local-storage"] input.mark-remove');
+  console.log(entries);
   
-  let entries = document.querySelectorAll('div[data="local-storage"]');
-  for (entry of entries) {
-    let checkBox = entry.firstChild;
-    if (checkBox.checked) {
-      removeButton.disabled = false;
-      return;
-    }
-  }
-  removeButton.disabled = true;
+  removeButton.disabled = ![...entries].some(entry => {
+    return entry.checked;
+  });
 }
 
 function repopulate() {
@@ -63,18 +76,23 @@ function repopulate() {
   
   
   chrome.storage.local.get(null, (entries) => {
+    console.log(entries);
     for (let [key, value] of Object.entries(entries)) {
       let entryContainer = document.createElement("div");
       entryContainer.setAttribute("data", "local-storage");
       let checkBox = document.createElement("input")
+      checkBox.classList.add("mark-remove");
       checkBox.setAttribute("type", "checkbox");
       checkBox.setAttribute("name", key);
       checkBox.addEventListener("change", handleRemove);
       entryContainer.appendChild(checkBox);
       let entryItem = document.createElement("ul");
+      entryItem.classList.add("stored-item");
       let entryKey = document.createElement("li");
+      entryKey.classList.add("stored-key");
       entryKey.textContent = key;
       let entryValue = document.createElement("li");
+      entryValue.classList.add("stored-value");
       entryValue.textContent = value;
       entryItem.appendChild(entryKey);
       entryItem.appendChild(entryValue);
@@ -84,6 +102,7 @@ function repopulate() {
   });
   
   expansionList.replaceWith(clone);
+  // expansionList.childNodes = clone;
   // expansionList.appendChild(clone);
 }
 
